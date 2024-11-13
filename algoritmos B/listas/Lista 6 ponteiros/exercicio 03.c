3f7e633e8327f7ad3575ee54f02ecf6eae3168cf
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:56:20 -0300

    Update exercicio 03.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 03.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 03.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
 
+void vertical(char s[]){
+char *pf,*ps,aux;
+
+pf = ps = s;
+
+
+    //laço para posicionar pf no \0
+    for( ;*pf != '\0'; pf++);
+
+    //posicionar o ponteiro na ultima letra
+    pf--;
+
+    //inverter a palavra
+    for( ;ps<pf; ps++,pf--){
+        aux = *ps;
+        *ps = *pf;
+        *pf = aux;
+    }
+
+    printf("Palavra invertida: %s", s);
+
+
+return ;
+}
+
+
+int main(){
+char nome[50],*p,*p2;
+
+printf("digite um nome:");
+gets(nome);
+
+vertical(nome);
+
+
+
+return 0;
+}
