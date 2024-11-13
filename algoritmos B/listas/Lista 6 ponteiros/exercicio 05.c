fbc2f6401b1dfb26f4e71ae66f3a254762610689
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:57:06 -0300

    Update exercicio 05.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 05.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 05.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
 
+int compara(char nome[], char palavra[]){
+    char *p,*p2;
+    p=nome;
+    p2=palavra;
+    for(;*p!='\0'||*p2!='\0';p++,p2++){
+    if(*p!=*p2){
+        return 1;
+    }
+    }
+return 2;
+}
+
+int main(){
+char nome[50],palavra[50];
+int comp;
+
+printf("digite um nome:");
+gets(nome);
+printf("digite um segundo nome:");
+gets(palavra);
+comp=compara(nome,palavra);
+if(comp==2){
+    printf("as palavras digitadas sao iguais.");
+}else{
+    printf("as palavras digitadas sao diferentes");
+}
+
+
+
+return 0;
+}
