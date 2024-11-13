93267ada6f4cfb22b7eb24251d12aa10d00ec4f5
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:56:01 -0300

    Update exercicio 02.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 02.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 02.c
@@ -1,1 +1,30 @@
 
+#include<stdio.h>
+
+int contador(char nome[]){
+char *p,a=0;
+p=&nome[0];
+
+
+for( ;*p!='\0'; p++){
+    a++;
+
+}
+return a;
+}
+
+
+int main(){
+char nome[50];
+int total;
+
+printf("digite um nome:");
+gets(nome);
+
+total=contador(nome);
+printf("o numero de caracteres na palavra digitada eh igual %i",total);
+
+
+
+return 0;
+}
