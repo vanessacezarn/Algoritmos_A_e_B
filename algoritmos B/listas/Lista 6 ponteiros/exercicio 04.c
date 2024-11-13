088fcd67c2df71c20c16d3529d4a5ad9a6acfd69
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:56:44 -0300

    Update exercicio 04.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 04.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 04.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
 
+int palindromo(char nome[]){
+    char *p,*pf,*aux;
+    pf=p=nome;
+
+    for(;*pf != '\0';pf++);
+
+    pf--;
+
+    for( ;p<pf; p++,pf--){
+            if(*p!= *pf){
+
+                return 1;
+            }
+
+    }
+    return 2;
+}
+
+
+
+int main(){
+char nome[50], *p;
+int pal;
+printf("digite uma palavra:");
+gets(nome);
+p=nome;
+pal=palindromo(p);
+if(pal==2){
+    printf("a palavra eh palindromo");
+}else{
+    printf("nao eh palindromo");
+}
+
+
+
+
+return 0;
+}
