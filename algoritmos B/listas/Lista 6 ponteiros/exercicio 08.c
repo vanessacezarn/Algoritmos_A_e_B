569e8ea360d8635b805dd43abdd81126ef417d5c
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:58:25 -0300

    Update exercicio 08.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 08.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 08.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+void contador(char nome[],char n){
+    int a=0;
+    char *p;
+    p=nome;
+    for(;*p!='\0';p++){
+        if(*p==n){
+            a++;
+        }
+    }
+    printf("a letra '%c' aparace %i vezes na palavra %s",n,a,nome);
+return ;
+}
 
+int main(){
+char nome[50],n;
+printf("digite um nome:");
+gets(nome);
+printf("digite uma letra:");
+scanf("%c",&n);
+contador(nome,n);
+return 0;
+}
