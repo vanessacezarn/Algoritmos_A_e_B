a1db008a1588277a2ad4ce95805dd708654d5461
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:50:20 -0300

    Update exercicio 01.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 01.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 01.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+void vertical(char nome[]){
+char *p;
+p=&nome[0];
+
+printf("nome na vertical:\n");
+for( ;*p!='\0'; p++){
+    printf("%c\n",*p);
+
+}
+return ;
+}
+
+
+int main(){
+char nome[50],*p,*p2;
+
+printf("digite um nome:");
+gets(nome);
+
+vertical(nome);
+
+
+
+return 0;
+}
