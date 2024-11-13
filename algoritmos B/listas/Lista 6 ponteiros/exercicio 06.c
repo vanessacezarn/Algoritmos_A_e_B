87aa5d067e23156b4490a7536aab23c15839665d
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-13 20:57:25 -0300

    Update exercicio 06.c

--- algoritmos B/listas/Lista 6 ponteiros/exercicio 06.c
+++ algoritmos B/listas/Lista 6 ponteiros/exercicio 06.c
@@ -1,1 +1,24 @@
 
+#include<stdio.h>
+void posicao(char nome[],char n){
+char *p;
+p=nome;
+for(;*p!='\0';p++){
+    if(*p==n){
+        *p='\0';
+    }
+}
+return ;
+}
+
+int main(){
+char n,nome[50];
+printf("digite uma palavra:");
+gets(nome);
+printf("digite uma letra:");
+scanf("%c",&n);
+posicao(nome,n);
+puts(nome);
+
+return 0;
+}
