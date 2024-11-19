635294e70ed7309019ffc44ef3ae024cc988bc44
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-19 17:08:03 -0300

    Update exercicio 02 - lê string letra por letra.c

--- algoritmos B/listas/Lista 7 ponteiros e alocação dinamica/exercicio 02 - lê string letra por letra.c
+++ algoritmos B/listas/Lista 7 ponteiros e alocação dinamica/exercicio 02 - lê string letra por letra.c
@@ -1,3 +1,8 @@
+//COMANDO DA QUESTÃO
+//Faça um programa que lê uma string, letra por letra, e ao final da execução mostra a string completa. Antes 
+de começar a ler as letras de string, peça para o usuário informar o tamanho da string e aloque 
+dinamicamente um vetor com o tamanho necessário.
+
 #include<stdio.h>
 #include<stdlib.h>
 
