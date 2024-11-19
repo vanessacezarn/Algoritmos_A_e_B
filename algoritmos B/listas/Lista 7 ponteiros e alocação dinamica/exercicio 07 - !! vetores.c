4e35f1c4004504bc625d0cb503097322f8025ef0
vanessacezarn <vanessacezar3449@gmail.com>
2024-11-19 17:16:50 -0300

    Update and rename exercicio 07.c to exercicio 07 - !! vetores.c

---
+++ algoritmos B/listas/Lista 7 ponteiros e alocação dinamica/exercicio 07 - !! vetores.c
@@ -0,0 +1,12 @@
+//COMANDO DA QUESTÃO
+//Um método de resolução de sistemas de equações lineares algébricas utiliza, em seu algoritmo, operações algébricas sobre matrizes
+//e vetores.Parte deste método é definido como: α=A⊗B,  A=αB,  α=A⊗A,  B=αA
+//onde: A e B são vetores, α é um escalar (número) e ⊗ é o produto escalar de dois vetores, obtido pela seguinte 
+//equação: ai ∗bi     n−1
+//                  A⊗B=∑ ❑
+//                    i=0 
+// Desenvolva o algoritmo para obter o valor final de  α, A e B. Considere que:
+   //- O tamanho dos vetores A e B são iguais e são informados pelo usuário;
+   //- Os valores iniciais de  α, A e B são informados pelo usuário;
+   //- Funções são utilizadas para leitura/exibição dos dados;
+   //- Deve existir uma função para cada tipo de cálculo.
