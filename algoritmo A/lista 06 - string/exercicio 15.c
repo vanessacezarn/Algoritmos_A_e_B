17fde3a72f0758d061e249defa7de9ba8f41a263
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:31:49 -0300

    Update exercicio 15.c

--- algoritmo A/lista 06 - string/exercicio 15.c
+++ algoritmo A/lista 06 - string/exercicio 15.c
@@ -1,1 +1,23 @@
 // 15. Criar uma string com todas as letras do alfabeto.
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char letra[30];
+    int i,x;
+
+    for(i=65,x=0;i<91;i++,x++){
+        printf("%c",i);
+        
+        letra[x]=i;
+    }
+   //para criar a string
+    letra[x]='\0';
+    printf("\n");
+    puts(letra);
+
+
+
+
+return 0;
+}
