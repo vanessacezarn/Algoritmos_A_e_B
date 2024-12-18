bf73dd1ccacd02bc6ac64daf7074f050d53e18b6
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:17:01 -0300

    Update exercicio 01.c

--- algoritmo A/lista 05b - matrizes/exercicio 01.c
+++ algoritmo A/lista 05b - matrizes/exercicio 01.c
@@ -1,3 +1,31 @@
 // 1. Implemente um algoritmo para transpor matrizes 2x3. Transpor uma matriz significa transformar suas
 // linhas em colunas e vice-versa.
 
+#include<stdio.h>
+int main(){
+  int a[2][3],b[3][2],i,j;
+
+  for(i=0;i<2;i++){
+    for(j=0;j<3;j++){
+      printf("Elemento [%d][%d]:",i,j);
+      scanf("%d",&a[i][j]);
+    }
+  }
+  printf("\n***MATRIZ***\n");
+  for(i=0;i<2;i++){
+    for(j=0;j<3;j++){
+      printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+  }
+printf("\n***MATRIZ TRANSPOSTA***\n");
+  for(i=0;i<3;i++){
+    for(j=0;j<2;j++){
+      b[i][j]=a[j][i];
+      printf("%d\t",b[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
