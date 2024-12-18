404ac9e9a569be4fbeb61a740105e741005097af
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:45:55 -0300

    Create exercicio 07.c

---
+++ algoritmo A/lista 02c/exercicio 07.c
@@ -0,0 +1,4 @@
+//7. Escreva um algoritmo para ler a idade de uma pessoa e mostrar qual sua situação de eleitor,
+// conforme as seguintes condições: se a idade for menor que 16 anos, informar que a pessoa não está
+// apta a votar. Se a idade for maior ou igual a 16 anos e menor que 18 anos ou superior a 70 anos,
+// informar que o voto é facultativo. Caso contrário, informar que o voto é obrigatório
