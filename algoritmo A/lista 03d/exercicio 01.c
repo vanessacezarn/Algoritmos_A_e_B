84d4b6326b0b55cb523d571aaa5fb09571bcbb6a
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:36:47 -0300

    Update exercicio 01.c

--- algoritmo A/lista 03d/exercicio 01.c
+++ algoritmo A/lista 03d/exercicio 01.c
@@ -7,3 +7,29 @@
 //escrever que não foi possível calcular as raízes. Se a = 0, ele deve escrever que a equação
 // é de primeiro grau.
 
+#include<stdio.h>
+int main(){
+  int a,b,c,x1,x2,delta;
+  printf("***BASCARA DE 3 NUMEROS DIGITADOS***\n");
+
+  while(a!=0 & b!=0 & c!=0){
+    printf("digite tres valores:");
+    scanf("%d%d%d",&a,&b,&c);
+  if(a==0){
+    printf("NÃO EH UMA EQUAÇÃO DO SEGUNDO GRAU\n");
+  }else{
+    delta=b*b-4*a*c;
+    printf("delta=%d\n",delta);
+    if(delta<0){
+    printf("NÃO FOI POSSIVEL CALCULAR AS RAIZES\n");
+    }else{
+    x1=-b+sqrt(delta)/2*a;
+    x2=-b-sqrt(delta)/2*a;
+    printf("x1=%d e x2=%d\n",x1,x2);
+  }
+  }
+  }
+
+
+  return 0;
+}
