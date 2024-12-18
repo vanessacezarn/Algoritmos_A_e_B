f1e0db6b55a15060855a88fdacdf7a95060ba664
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:24:24 -0300

    Delete algoritmo A/lista 04 directory

--- algoritmo A/lista 04/exercicio 10.c
+++
@@ -1,4 +0,0 @@
- //10.Escreva um algoritmo para ler um vetor de inteiros e positivos e imprimir quantas vezes
- // aparecem o números 2, 4 e 8. O vetor terá no máximo 100 posições. Sair do programa
-// quando for digitado -1 ou quando atingir o máximo de posições do vetor.
-
