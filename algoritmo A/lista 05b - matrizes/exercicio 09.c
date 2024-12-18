ee6768b7d73fb9f6964757a20f6fb4889b7c4e60
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:13:35 -0300

    Create exercicio 09.c

---
+++ algoritmo A/lista 05b - matrizes/exercicio 09.c
@@ -0,0 +1,5 @@
+// 9. Desenvolva um algoritmo para gerar uma matriz 4x4 e, depois, calcular e mostrar:
+// - O somatório dos elementos da primeira coluna
+// - O valor da multiplicação dos elementos da primeira linha
+// - A soma da diagonal principal
+// -A soma de todos os elementos da matriz
