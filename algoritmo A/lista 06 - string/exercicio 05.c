8dd1630c108d7d4f76470e44be83f3ba39c6325b
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:44:49 -0300

    Update exercicio 05.c

--- algoritmo A/lista 06 - string/exercicio 05.c
+++ algoritmo A/lista 06 - string/exercicio 05.c
@@ -1,2 +1,23 @@
  //5. Ler uma palavra e uma letra qualquer. Mostrar a palavra cortada na primeira posição em que encontrar a
  //letra informada.
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i;
+    char nome[50],n;
+
+    printf("digite uma palavra:");
+    gets(nome);
+
+    printf("digite uma letra:");
+    scanf("%c",&n);
+
+    for(i=0;nome[i]!='\0';i++){
+        if(nome[i]==n){
+            nome[i]='\0';
+        }
+    }
+puts(nome);
+return 0;
+}
