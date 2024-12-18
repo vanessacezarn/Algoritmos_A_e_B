b345f7d99e403e2bfc7dfdaef921ae9764588079
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:53:13 -0300

    Create exercicio 04.c

---
+++ algoritmo A/lista 5/exercicio 04.c
@@ -0,0 +1,6 @@
+//4. Em um concurso, a identificação das cadeiras da sala são armazenadas em uma matriz. Considere que
+//existem 6 filas de cadeiras na sala e cada fila possui 7 cadeiras. Desenvolva um algoritmo com uma matriz
+//para armazenar a identificação das cadeiras que é informada pelo usuário. A matriz deve ser exibida. Após,
+//apresentar ao usuário a mensagem: “Insira o número da fila:” para que o algoritmo leia o número de uma
+//fila e mostra a identificação de todas as cadeiras desta fila.
+
