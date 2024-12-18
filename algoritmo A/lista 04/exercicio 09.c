14f47c0fc8cc042076e77f249bf9ea459963d337
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:57:14 -0300

    Create exercicio 09.c

---
+++ algoritmo A/lista 04/exercicio 09.c
@@ -0,0 +1,3 @@
+//9. Escreva um algoritmo que leia um vetor de 20 posições e mostre-o. Em seguida, troque o
+//primeiro elemento com o último, o segundo com o penúltimo, o terceiro com o
+// antepenúltimo e assim sucessivamente. Mostre o novo vetor depois da troca.
