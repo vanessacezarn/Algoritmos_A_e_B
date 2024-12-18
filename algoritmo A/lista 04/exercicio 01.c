c4a07dd7ee2462ec341adc03b6e66496a61bcae6
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:59:41 -0300

    Update exercicio 01.c

--- algoritmo A/lista 04/exercicio 01.c
+++ algoritmo A/lista 04/exercicio 01.c
@@ -1,2 +1,19 @@
 //1. Escreva um algoritmo que leia e mostre um vetor de 20 elementos inteiros. A seguir,
 // conte quantos valores pares existem no vetor.
+
+#include<stdio.h>
+int main(){
+  int a[20],i,qtde=0;
+
+    for(i=0;i<20;i++){
+      printf("digite um numero:");
+      scanf("%d",&a[i]);
+    
+    if(a[i]%2==0){
+      qtde++;
+    }
+    }
+    printf("quantidade de numeros pares:%d\n",qtde);
+    
+  return 0;
+}
