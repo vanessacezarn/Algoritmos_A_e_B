6e45205161e606c52123026046654d16869962b5
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:54:14 -0300

    Update exercicio 08.c

--- algoritmo A/lista 02c/exercicio 08.c
+++ algoritmo A/lista 02c/exercicio 08.c
@@ -5,3 +5,40 @@
 // 6 a 8 anos        M        Infantil A-M
 // 9 a 11 anos     F ou M     Infantil B
 // 12 a 14 anos    F ou M       Juvenil 
+
+#include<stdio.h>
+int main(){
+  char sexo;
+  int idade,inscricao;
+  printf("digite o sexo do nadador(F-feminino ou M-masculino):");
+  scanf(" %c",&sexo);
+  printf("digite a inscricao do nadador:");
+  scanf("%d",&inscricao);
+  printf("digite a idade do nadador:");
+  scanf("%d",&idade);
+
+  if(idade<6){
+    printf("OPCAO INVALIDA");
+    }else{
+         if(idade>14){
+    printf("OPCAO INVALIDA");
+      }
+    }
+  if(sexo=='F'&& idade>=6 && idade <= 8){
+    printf("CATEGOTIA INFANTIAL A-F");
+  }else{
+       if(sexo=='M'&& idade>=6 && idade <= 8){
+         printf("CATEGORIA INFANTIL A-M");
+       }else{
+          if(sexo=='F'||sexo=='M'&& idade>=9 && idade <= 11){
+          printf("CATEGORIA JUVENIL B");
+          }else{
+           if(sexo=='F'||sexo=='M'&& idade>=12 && idade <= 14){
+             printf("CATEGORIA JUVENIL A");
+           }
+        }
+    
+  }
+  }
+  return 0;
+}
