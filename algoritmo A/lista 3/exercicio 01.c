f26991650ae41015bf9f87d95497721a7322653c
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:42:08 -0300

    Update exercicio 01.c

--- algoritmo A/lista 3/exercicio 01.c
+++ algoritmo A/lista 3/exercicio 01.c
@@ -1,1 +1,25 @@
+// 1. Ler dois números e mostrar os números pares do intervalo
 
+#include<stdio.h>
+int main(){
+    int a,b,auxiliar,i;
+  printf("digite dois numeros inteiros:");
+  scanf("%i%i",&a,&b);
+
+  if(a>b){
+    auxiliar=a;
+    a=b;
+    b=auxiliar;
+  }
+  printf("os numeros pares do intervalo entre %i e %i são:",a,b);
+  for(i=a;i<=b;i++) {
+    if(i%2==0){
+      printf("\t%i",i);
+    }
+  }
+
+
+
+
+  return 0;
+}
