a7e74a22e7dbab8d5ec08f1753d54f7ebede15f9
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:41:22 -0300

    Update exercicio 06.c

--- algoritmo A/lista 06 - string/exercicio 06.c
+++ algoritmo A/lista 06 - string/exercicio 06.c
@@ -1,1 +1,21 @@
 // 6. Ler uma palavra e substituir todas as vogais por #. Mostar a palavra modificada.
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i;
+    char nome[50];
+
+    printf("digite uma palavra:");
+    gets(nome);
+
+    for(i=0;nome[i]!='\0';i++){
+        if(toupper(nome[i])=='A'||toupper(nome[i])=='E'||toupper(nome[i])=='I'|| toupper(nome[i])=='O'||toupper(nome[i])=='U'){
+            nome[i]= '#';
+        }
+    }
+    puts(nome);
+
+
+    return 0;
+}
