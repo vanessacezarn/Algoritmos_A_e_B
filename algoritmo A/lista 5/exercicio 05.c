4ab2d55ecca95257d8986ca2c1cf8285fa819eac
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:57:17 -0300

    Update exercicio 05.c

--- algoritmo A/lista 5/exercicio 05.c
+++ algoritmo A/lista 5/exercicio 05.c
@@ -8,3 +8,50 @@
 // b) Leia os dados armazenados em cada área
 // c) Calcule e exiba a média de produtividade da lavoura
 // d) Identifique e mostre as áreas que produziram acima da média e abaixo da média
+
+
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    float soma=0,media,m[2][4];
+
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++){
+            printf("digite a produtividade da area [%d][%d]:",i,j);
+            scanf("%f",&m[i][j]);
+        }
+    }
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++){
+                printf("%.2f\t",m[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMEDIA DA PRODUTIVIDADE:");
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++){
+            soma=soma+m[i][j];
+        }
+    }
+    media=soma/8;
+    printf("%.2f",media);
+
+    printf("\nAREAS COM PRODUTIVIDADE MAIOR QUE A MEDIA:");
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++){
+            if(m[i][j]>media){
+                printf("\tA%d%d",i,j);
+            }
+        }
+    }
+    printf("\nAREAS COM PRODUTIVIDADE MENOR QUE A MEDIA:");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            if(m[i][j]<media){
+                printf("\tA%d%d",i,j);
+            }
+        }
+    }
+
+    return 0;
+    }
