f45f075a5ddee6b2803ad1b6dadf34c95a8d30a2
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:41:56 -0300

    Update exercicio 01.c

--- algoritmo A/lista 02c/exercicio 01.c
+++ algoritmo A/lista 02c/exercicio 01.c
@@ -1,1 +1,7 @@
-
+//1. A empresa XYZ decidiu dar um aumento a seus funcionários de acordo com a seguinte regra:
+//-13% para os salários inferiores ou iguais a R$ 2000,00;
+//-11% para os salários situados entre R$ 2000,0 e R$ 4000,00 (inclusive);
+//-9 % para os salários entre R$ 4000,00 e R$ 8000,00 (inclusive); e
+//-7% para os demais salários.
+// Desenvolva um programa que receba o salário atual de um funcionário e forneça o valor do
+// seu novo salário. Mostre também o bônus (em reais e em %).
