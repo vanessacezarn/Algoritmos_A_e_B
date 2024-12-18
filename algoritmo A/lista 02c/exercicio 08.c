24edba1ab574215f1b765783d5aac66e517d734e
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:48:14 -0300

    Create exercicio 08.c

---
+++ algoritmo A/lista 02c/exercicio 08.c
@@ -0,0 +1,7 @@
+//8. Escreva um algoritmo que leia o número de inscrição de um nadador, sua idade e sexo (F
+//ou M). O algoritmo deve escrever a categoria do nadador, conforme a tabela:
+//   Idade         Sexo      Categoria
+// 6 a 8 anos        F        Infantil A-F
+// 6 a 8 anos        M        Infantil A-M
+// 9 a 11 anos     F ou M     Infantil B
+// 12 a 14 anos    F ou M       Juvenil 
