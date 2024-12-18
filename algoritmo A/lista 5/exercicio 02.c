f8fd1009d39ef9a652956fbed409ea92261cdb72
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:51:47 -0300

    Create exercicio 02.c

---
+++ algoritmo A/lista 5/exercicio 02.c
@@ -0,0 +1,7 @@
+ //2. Escreva um programa em C que gere os elementos de uma matriz quadrada 5 x 5 e:
+ // - Exiba todos os elementos da matriz
+ // - Exiba os elementos da diagonal principal da matriz
+ // - Calcule e mostre a média dos valores da diagonal principal da matriz
+ // - Calcule e mostre a média dos elementos da matriz
+ // - Encontre e mostre na tela os elementos que estão acima da média, com sua posição (índice de linha e
+ coluna)
