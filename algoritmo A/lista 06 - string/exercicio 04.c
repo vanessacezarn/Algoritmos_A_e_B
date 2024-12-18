c34d6e92c51f6144effaa2184df545f5a28023ce
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:44:18 -0300

    Update exercicio 04.c

--- algoritmo A/lista 06 - string/exercicio 04.c
+++ algoritmo A/lista 06 - string/exercicio 04.c
@@ -1,2 +1,23 @@
 //4. Escreva um programa que leia uma palavra qualquer e escreva como resultado a mesma palavra na ordem
 // inversa.
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i,tamanho=0;
+    char nome[50];
+
+    printf("digite uma palavra:");
+    gets(nome);
+
+    for(i=0;nome[i]!= '\0';i++){
+            tamanho++;
+    }
+
+    printf("\npalavra na ordem inversa:");
+    for(i=tamanho;i>=0;i--){
+        printf("%c",nome[i]);
+    }
+
+return 0;
+}
