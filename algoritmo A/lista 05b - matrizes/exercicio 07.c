dfad465f46faa24c337088af9eff0f3fb910fb5c
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:12:11 -0300

    Create exercicio 07.c

---
+++ algoritmo A/lista 05b - matrizes/exercicio 07.c
@@ -0,0 +1,3 @@
+ //7. Gere uma matriz A, 10 x 10, e mostre ao usuário. A seguir, leia um número diferente de zero e gere uma
+ //nova matriz B, 10 x 10, sendo que cada elemento de B é obtido pela divisão do elemento correspondente de
+ //A pelo número informado.
