d08ddffdd02ef3b6c5308205346185fd13efa2e9
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:14:04 -0300

    Update exercicio 11.c

--- algoritmo A/lista 04/exercicio 11.c
+++ algoritmo A/lista 04/exercicio 11.c
@@ -2,3 +2,36 @@
 // números. Se o código for zero, termine o algoritmo. Se o código for 1, mostre o vetor na
 // ordem em que foi lido. Se o código for 2, mostre o vetor na ordem inversa, do último
 // elemento até o primeiro.
+
+#include<stdio.h>
+int main(){
+    int codigo,v[6],i,a;
+ 
+  printf("digite os valores do vetor:");
+  for(i=0;i<6;i++){
+    scanf("%i",&v[i]);
+  }
+    printf("digite o codigo:");
+    scanf("%d",&codigo);
+      if(codigo==0){
+        printf("programa encerrado");
+      }else
+        if(codigo==1){
+        for(i=0;i<6;i++)
+      printf("%d\t",v[i]);
+        } 
+      else{
+      if(codigo==2){
+        for(i=0;i<3;i++){
+          a=v[i];
+          v[i]=v[5-i];
+          v[5-i]=a;
+        }
+        printf("na ordem inversa:");
+        for(i=0;i<6;i++)
+          printf("%i\t",v[i]);
+      }
+   
+  }
+  return 0;
+}
