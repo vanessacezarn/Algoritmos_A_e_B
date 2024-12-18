0175bb7ac483b8feced503e7df1a4e4d65e897f7
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:51:03 -0300

    Update exercicio 03.c

--- algoritmo A/lista 02c/exercicio 03.c
+++ algoritmo A/lista 02c/exercicio 03.c
@@ -1,2 +1,19 @@
 //3. Ler a idade de uma pessoa expressa em anos, meses e dias. O algoritmo deve mostrar a
 // idade da pessoa em dias (considere que todos os anos tem 365 dias e todos os meses tem 30 dias).
+
+#include<stdio.h>
+int main(){
+//declaraçao de variaveis
+int idade_em_anos,meses,dias,idade_em_dias;
+//entrada de dados
+printf("digite o valor de idade_em_anos,meses,dias:");
+scanf("%i%i%i",&idade_em_anos,&meses,&dias);
+//calculos
+idade_em_dias=(idade_em_anos*365)+(meses*30)+dias;
+//saida de dados
+printf("idade_em_dias=%i\n",idade_em_dias);
+
+
+
+return 0;
+}
