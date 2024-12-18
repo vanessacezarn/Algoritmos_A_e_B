1b5cd10a528ed6b3f486815d4ff2ddbe6cbc8a1c
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:56:44 -0300

    Create exercicio 08.c

---
+++ algoritmo A/lista 04/exercicio 08.c
@@ -0,0 +1,3 @@
+//8. Faça um algoritmo que leia um vetor V de 10 posições e, após, verifica se um número N,
+//fornecido pelo usuário, existe no vetor. Se existir, indicar a(s) posição(ões), senão
+// escrever a mensagem "O número fornecido não existe no vetor!".
