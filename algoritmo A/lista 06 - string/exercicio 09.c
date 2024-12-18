4fc61a4126190e4b7c6a001f949a970f07477164
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:27:54 -0300

    Create exercicio 09.c

---
+++ algoritmo A/lista 06 - string/exercicio 09.c
@@ -0,0 +1,3 @@
+// 9. Ler duas palavras e compará-las. O programa deve informar se as palavras são iguais, em caso contrário,
+// informar se a primeira é maior do que a segunda, se a segunda é maior do que a primeira ou se são
+// diferentes e tem o mesmo tamanho.
