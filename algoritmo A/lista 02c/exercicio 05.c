23c2e3171bffe896f3896e68b0533ce141524f5e
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:44:30 -0300

    Create exercicio 05.c

---
+++ algoritmo A/lista 02c/exercicio 05.c
@@ -0,0 +1,3 @@
+//5. Fazer um algoritmo que leia valores para as variáveis hora, minuto e segundo; verificando e
+// escrevendo se elas correspondem a um horário válido ou não. Para um horário ser válido, a hora
+// deve estar no intervalo de 0 a 23, o minuto e o segundo no intervalo de 0 a 59.
