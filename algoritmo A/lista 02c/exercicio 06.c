73778c79f4127ff856a8d1521325339f6607a48d
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:45:20 -0300

    Create exercicio 06.c

---
+++ algoritmo A/lista 02c/exercicio 06.c
@@ -0,0 +1,8 @@
+ //6. Determine e informe a faixa etária de uma pessoa, a partir do seu ano de nascimento e do
+ //ano atual. Faixas:
+//- 0 a 9 anos: criança
+//-10 a 11 anos: pré-adolescente
+//- 12 a 18 anos: adolescente
+//- 18 a 25 anos: jovem
+//- 26 a 59 anos: adulto
+//- 60 em diante: idoso
