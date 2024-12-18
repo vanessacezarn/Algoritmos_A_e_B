3e515bbe4c8622c642717c77c52a90ba50b19209
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:43:18 -0300

    Update exercicio 02.c

--- algoritmo A/lista 06 - string/exercicio 02.c
+++ algoritmo A/lista 06 - string/exercicio 02.c
@@ -1,2 +1,18 @@
 //2. Escreva um programa que leia uma string e mostre ao usuário quantos caracteres possui (tamanho da
 // string).
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i;
+    char nome[40];
+
+    printf("digite uma palavra:");
+    gets(nome);
+
+    for(i=0;nome[i]!= '\0';i++){
+    }
+
+    printf("a palavra possui %d caracteres\n",strlen(nome));
+return 0;
+}
