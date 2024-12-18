98dceebd5e9ce26ca798c62c641e425d5e156c6d
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:43:43 -0300

    Create exercicio 04.c

---
+++ algoritmo A/lista 02c/exercicio 0,4.c
@@ -0,0 +1,3 @@
+//4. Escreva um programa que pede para o usuário digitar um número que corresponda a um
+//dia da semana. O programa deve apresentar na tela o nome do dia. Utilize o comando
+//switch.
