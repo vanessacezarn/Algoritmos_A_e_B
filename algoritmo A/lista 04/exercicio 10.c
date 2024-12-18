5f983e7a65d1f2e4413b18e285446471d9c4cf9a
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:12:25 -0300

    Update exercicio 10.c

--- algoritmo A/lista 04/exercicio 10.c
+++ algoritmo A/lista 04/exercicio 10.c
@@ -1,3 +1,20 @@
  //10.Escreva um algoritmo para ler um vetor de inteiros e positivos e imprimir quantas vezes
  // aparecem o números 2, 4 e 8. O vetor terá no máximo 100 posições. Sair do programa
 // quando for digitado -1 ou quando atingir o máximo de posições do vetor.
+
+#include<stdio.h>
+int main(){
+    int x[10],y[10],i,u[20],d[10],in[10];
+    printf("digite os valores para o vetor x:");
+    for(i=0;i<10;i++){
+        scanf("%d",&x[i]);
+    }
+    printf("digite os valore para o vetor y:");
+    for(i=0;i<10;i++){
+        scanf("%d",&y[i]);
+    }
+
+
+
+return 0;
+}
