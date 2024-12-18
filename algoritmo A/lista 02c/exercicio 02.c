1bbc1d15f85394ed55418ce4884ab93c21415eba
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:42:29 -0300

    Create exercicio 02.c

---
+++ algoritmo A/lista 02c/exercicio 02.c
@@ -0,0 +1,3 @@
+//2. Desenvolva um programa para ler um número inteiro de 1 a 12, que corresponde a um dos
+// meses do ano, e outro número inteiro que corresponde ao ano. Encontre e mostre o número de dias
+// referente ao mês informado.
