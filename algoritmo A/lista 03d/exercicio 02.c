21529b705aaff363c63be42130712d7843bee3f6
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:32:29 -0300

    Create exercicio 02.c

---
+++ algoritmo A/lista 03d/exercicio 02.c
@@ -0,0 +1,6 @@
+// 2. Uma determinada região está sendo desmatada a uma taxa percentual constante, ano após
+// ano. Escreva um algoritmo que leia a área da região, a taxa de desmatamento anual e
+// escreva o ano em que a mata estará reduzida a menos de 20% de sua atual área.
+// Por exemplo, se a área de floresta é 10.000.000 km2 e a taxa de desmatamento é de 1,5%
+// ao ano, o algoritmo deve escrever como saída:
+// “Em 2131, a área estará reduzida a menos ou igual a 20% de sua área atual, o que corresponde a 1984616 km2.”
