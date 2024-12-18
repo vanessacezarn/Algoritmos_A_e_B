7fb7ad1283a4c94ad1b51fa0f319c39b39bfc985
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:07:27 -0300

    Update exercicio 06.c

--- algoritmo A/lista 04/exercicio 06.c
+++ algoritmo A/lista 04/exercicio 06.c
@@ -1,2 +1,17 @@
 // 6. Escreva um programa que leia valores em um vetor de 5 posições. Escrever os elemento
 //do vetor e após escrever os elementos na ordem inversa.
+#include<stdio.h>
+int main(){
+  int a[5],i,b[5];
+  printf("digite os 5 elementos de um vetor:");
+  for(i=0;i<5;i++){
+      scanf("%d",&a[i]);
+  }
+  for(i=4;i>=0;i--){
+    printf("\n%d\n",a[i]);
+  
+  }
+  printf("os elementos na ordem inversa sao");
+
+  return 0;
+}
