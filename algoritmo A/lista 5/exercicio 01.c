1bbb169e4bffa807a335b09c8d4f132aa111ade9
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:49:58 -0300

    Update exercicio 01.c

--- algoritmo A/lista 5/exercicio 01.c
+++ algoritmo A/lista 5/exercicio 01.c
@@ -1,1 +1,20 @@
+//1. Ler uma matriz informada pelo usuário, com tamanho 3 x 5. Mostrar seus elementos.
 
+#include<stdio.h>
+int main(){
+    int m[3][5],i,j,n;
+
+    for(i=0;i<3;i++){
+        for(j=0;j<5;j++){
+            printf("digite o elemento [%d][%d]:",i,j);
+            scanf("%d",&m[i][j]);
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<5;j++){
+                printf("%d\t",m[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+    }
