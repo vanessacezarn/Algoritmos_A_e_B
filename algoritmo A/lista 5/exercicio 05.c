c642906a9f072021e68e00c8cf5b6519cc7f3620
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 00:55:02 -0300

    Create exercicio 05.c

---
+++ algoritmo A/lista 5/exercicio 05.c
@@ -0,0 +1,10 @@
+//Um produtor organizou as áreas de sua lavoura da seguinte forma:
+// A00  A01  A02  A03
+// A10  A11  A12  A13
+// Em cada área, ele aplicou diferentes formas de tratamento do solo e das plantas (insumos, pesticidas...).
+// Após realizar a colheita, ele armazenou os dados da produtividade (quantidade de sacos/hectare) de cada
+// uma das áreas.
+// a) Utilize uma matriz para armazenar os valores da produtividade de cada área
+// b) Leia os dados armazenados em cada área
+// c) Calcule e exiba a média de produtividade da lavoura
+// d) Identifique e mostre as áreas que produziram acima da média e abaixo da média
