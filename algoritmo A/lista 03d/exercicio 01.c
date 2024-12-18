fb69280a2a30093ceba203fe5cc7514dc7d08377
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:31:24 -0300

    Update exercicio 01.c

--- algoritmo A/lista 03d/exercicio 01.c
+++ algoritmo A/lista 03d/exercicio 01.c
@@ -1,1 +1,9 @@
+//1. Encontre as raízes de uma equação de 2º grau ax2 + bx + c. O algoritmo deverá ler os 
+//coeficientes a, b e c e calcular as raízes utilizando a fórmula de Báskara:
+// O algoritmo deve ficar lendo os valores dos coeficientes a, b e c até que os valores
+// fornecidos para os três coeficientes sejam 0 (zero). Para cada conjunto de valores de
+// coeficientes lidos, o algoritmo deverá calcular e escrever os valores das duas raízes x1 e
+//x2. Se as raízes forem imaginárias (∆ negativo, sendo ∆ = b2 - 4ac ) o algoritmo deverá
+//escrever que não foi possível calcular as raízes. Se a = 0, ele deve escrever que a equação
+// é de primeiro grau.
 
