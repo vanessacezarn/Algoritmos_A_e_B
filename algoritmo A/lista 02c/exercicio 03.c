ee1b4a161b94c321eb4c6d4a576a940043f8c1ff
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:43:00 -0300

    Create exercicio 03.c

---
+++ algoritmo A/lista 02c/exercicio 03.c
@@ -0,0 +1,2 @@
+//3. Ler a idade de uma pessoa expressa em anos, meses e dias. O algoritmo deve mostrar a
+// idade da pessoa em dias (considere que todos os anos tem 365 dias e todos os meses tem 30 dias).
