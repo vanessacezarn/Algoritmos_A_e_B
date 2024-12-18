b9da995b6a7e3d7c8a37734a85f15a931e9c7d2b
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:43:47 -0300

    Update exercicio 03.c

--- algoritmo A/lista 06 - string/exercicio 03.c
+++ algoritmo A/lista 06 - string/exercicio 03.c
@@ -1,1 +1,18 @@
 // 3. Faça um programa que lê uma string e escreve esta string verticalmente na tela.
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i;
+    char nome[40];
+
+    printf("digite uma palavra:");
+    gets(nome);
+
+    printf("a palavra escrita verticalmente:\n");
+    for(i=0;nome[i]!='\0';i++){
+        printf("%c\n",nome[i]);
+    }
+
+return 0;
+}
