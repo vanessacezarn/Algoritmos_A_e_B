25a650aa516f0123a4a7aacee50d347492935d06
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 02:33:03 -0300

    Create exercicio 03.c

---
+++ algoritmo A/lista 03d/exercicio 03.c
@@ -0,0 +1,3 @@
+// 3. Escreva um algoritmo que imprima a tabela de equivalência de graus Fahrenheit para
+// Celsius (centígrados). Os limites são de 50 a 70 graus Fahrenheit, com intervalo de 1
+// grau. A fórmula para conversão de Fahrenheit (F) para Celsius (C) é
