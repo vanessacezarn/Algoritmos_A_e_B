bddcfff35bcc6ff28d59c0976082f9b065e91111
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-18 01:20:47 -0300

    Update exercicio 07.c

--- algoritmo A/lista 05b - matrizes/exercicio 07.c
+++ algoritmo A/lista 05b - matrizes/exercicio 07.c
@@ -1,3 +1,31 @@
  //7. Gere uma matriz A, 10 x 10, e mostre ao usuário. A seguir, leia um número diferente de zero e gere uma
  //nova matriz B, 10 x 10, sendo que cada elemento de B é obtido pela divisão do elemento correspondente de
  //A pelo número informado.
+
+#include <stdio.h>
+int main(){
+  int i,j,a[4][4],soma=0,multi=1,diagonal=0,matriz=0;
+  float b[4][4],n;
+
+  for(i=0;i<4;i++){
+    for(j=0;j<4;j++){
+      a[i][j]=i+j+2;
+      printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("digite um numero:");
+  scanf("%f",&n);
+
+  printf("NOVA MATRIZ\n");
+  for(i=0;i<4;i++){
+    for(j=0;j<4;j++){
+      b[i][j]=a[i][j]/n;
+      printf("%.2f\t",b[i][j]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
