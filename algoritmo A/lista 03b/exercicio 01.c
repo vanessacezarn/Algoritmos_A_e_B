2adb80162995900171a49c299c7061aec5dc62b8
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-25 20:54:09 -0300

    Update exercicio 01.c

--- algoritmo A/lista 03b/exercicio 01.c
+++ algoritmo A/lista 03b/exercicio 01.c
@@ -1,1 +1,20 @@
+//Faça um algoritmo para calcular e escrever a tabuada dos números de 1 a 10.
 
+#include<stdio.h>
+int main(){
+    int a,b,r;
+  printf("***TABUADA DO 1 AO 10***\n");
+
+  for(a=1;a<=10;a++){
+      printf("\n***TABUDA DO %d***",a);
+      for(b=1;b<=10;b++){
+        r=a*b;
+        printf("\n%i X %d = %d\t",a,b,r);
+        
+      }
+  }
+
+
+
+  return 0;
+}
