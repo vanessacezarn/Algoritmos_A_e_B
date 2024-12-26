d200888176224e40170281aac539ab205ec414dd
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-25 21:30:11 -0300

    Create exercicio 12.c

---
+++ algoritmo A/lista 04 - vetor/exercicio 12.c
@@ -0,0 +1,3 @@
+//Escreva um algoritmo que leia 2 vetores X(10) e Y(10) e os escreva. Crie, a seguir, um vetor  para cada uma das operações:
+//• A união de X com Y    • A diferença entre X e Y    • A interseção entre X e Y
+// Escreva os vetores resultado de cada operação
