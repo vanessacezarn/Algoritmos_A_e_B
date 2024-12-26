e1a9b619c8c9b449be23f8a1f1f45506316bfd2b
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-25 21:31:17 -0300

    Create exercicio 14.c

---
+++ algoritmo A/lista 04 - vetor/exercicio 14.c
@@ -0,0 +1,3 @@
+//Faça um programa em C que declare um vetor de 20 elementos inteiros, leia os conteúdos do vetor, e copie estes conteúdos para outro 
+//vetor, invertendo sua ordem. Assim, o valor do primeiro elemento do primeiro vetor deve ser o valor do último elemento do segundo
+// vetor, por exemplo. Mostrar os conteúdos do primeiro vetor em uma linha e os do segundo uma linha abaixo.
