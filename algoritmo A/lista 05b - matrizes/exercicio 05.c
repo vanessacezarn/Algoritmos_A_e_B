bf0027b1e172109af716683325c3b05785ddfc7a
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-25 21:34:30 -0300

    Create exercicio 05.c

---
+++ algoritmo A/lista 05b - matrizes/exercicio 05.c
@@ -0,0 +1,2 @@
+//Faça um algoritmo para ler uma matriz 3x4 e peça ao usuário para informar o número de uma linha da matriz. Armazene a linha 
+//informada da matriz em um vetor e, por fim, mostre o vetor. 
