250dcf6bb5730533361e571ad69da953c7f3f70b
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-25 21:36:33 -0300

    Create exercicio 08.c

---
+++ algoritmo A/lista 05b - matrizes/exercicio 08.c
@@ -0,0 +1,6 @@
+// Escrever um programa para ler uma matriz 4x4 de números reais, e depois determinar se ela se enquadra em uma das seguintes classes
+//de matrizes:
+//Matriz simétrica: A[i][j] = A[j][i], para todo i e j
+//Matriz diagonal:  A[i][j] = 0, para i != j
+//Matriz triangular superior: A[i][j] = 0, para i < j
+//Matriz triangular inferior: A[i][j] = 0, para i > j
