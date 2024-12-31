2e7b6dae13bcf2f22abeb87572c09ac7add7adcb
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-31 00:13:02 -0300

    Create questao 02.c

---
+++ algoritmos B/produto-de-aprendizagem/produto 02/questao 02.c
@@ -0,0 +1,11 @@
+//Desenvolva um programa para manipular uma matriz de tamanho NxN, em que N é definido pelo usuário e  apresente um menu com as 
+//opções: 1 – Informar os elementos da Matriz (neste caso, o usuário irá digitar os valores)
+// 2 – Gerar os elementos da Matriz (neste caso, o programa gera randomicamente os valores) Posteriormente, apresente um Menu com as
+//seguintes opções: A – Classe de matriz B – Cálculos sobre a matriz C – Sair 
+//Se for selecionada a opção A, determine se a matriz se enquadra em uma das seguintes classes de matrizes:
+// -Matriz simétrica: A[i][j] = A[j][i], para todo i e j - Matriz diagonal: A[i][j] = 0, para i != j
+// Matriz triangular superior: A[i][j] = 0, para i < j - Matriz triangular inferior: A[i][j] = 0, para i > j
+// Se for selecionada a opção B,  calcule e mostre:  O somatório dos elementos da primeira coluna  O valor da multiplicação dos 
+//elementos da primeira linha, por um número inteiro   A soma da diagonal principal   O maior valor da matriz e sua posição (índice
+//da linha e da coluna). Se houver valores iguais, mostrar todas as posições do maior valor.
+// Se for selecionada a opção C,  o programa deve ser encerrado
