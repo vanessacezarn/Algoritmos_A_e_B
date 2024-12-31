e37ceeaa70975002f1b9276b5245f66494862125
vanessacezarn <vanessacezar3449@gmail.com>
2024-12-31 00:13:57 -0300

    Update questao 01.c

--- algoritmos B/produto-de-aprendizagem/produto 03/questao 01.c
+++ algoritmos B/produto-de-aprendizagem/produto 03/questao 01.c
@@ -1,1 +1,4 @@
-
+//Desenvolva um programa, na linguagem C, para implementar o Jogo da Forca: jogo de adivinhar uma palavra, escolhendo letras que
+//podem estar contidas na palavra. Como estamos trabalhando a interface em modo texto, use a criatividade para representar quantas
+//chances o usuário terá para adivinhar a palavra. Para o desenvolvimento deste programa, devem ser utilizados os conteúdos 
+//trabalhados durante o semestre: funções, passagem de parâmetros, strings, ponteiros, alocação dinâmica de memória e arquivos.
